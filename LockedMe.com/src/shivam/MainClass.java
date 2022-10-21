44e9fdf65a3fe69b1cd2fe60ce2c290db93b6874
Shivam23396 <116022033+Shivam23396@users.noreply.github.com>
2022-10-21 21:59:32 +0530

    Add files via upload

---
+++ LockedMe.com/src/shivam/MainClass.java
@@ -0,0 +1,17 @@
+package shivam;
+
+public class MainClass {
+
+	public static final String path = "C:\\Users\\Shivam\\Desktop\\New folder";
+
+	public static void main(String[] args) {
+
+		Features features = new Features();
+		System.out.println("***************************************************************************");
+		features.welcomeScreen();
+		System.out.println("***************************************************************************");
+		features.mainMenu();
+
+	}
+
+}
